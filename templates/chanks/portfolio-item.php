e08c9cb0156d09896511f782e4a9a3d0bc498d7f
Evgenii <dcs-spb@ya.ru>
2020-04-16 07:16:30 +0300

    Stable update

--- templates/chanks/portfolio-item.php
+++ templates/chanks/portfolio-item.php
@@ -53,3 +53,61 @@ foreach($screenshot_mobile as $v){
 
 
 
+<!--						
+										<div itemprop="headline" class="h5 mkdf-portfolio-item-title"><?php echo $md_portfolio['meta_h1'];?></div>
+										<div class="mkdf-portfolio-item-categories">
+											<div class="h6"><?php echo $md_portfolio['project'];?></div> 
+										</div>
+										<div itemprop="description" class="mkdf-portfolio-item-content">
+											<p><?php echo $md_portfolio['content'];?></p>
+										</div>
+									</div>
+									<div class="mkdf-portfolio-info-item mkdf-portfolio-date">
+										<div class="h6">Дата</div>
+										<p>
+											<time itemprop="datePublished" datetime="<?php echo $md_portfolio['public_time'];?>"> <?php echo $public_time;?> </time>
+										</p>
+									</div>
+									
+
+
+
+					
+					
+					
+						<div class="mkdf-portfolio-prev mkdf-single-nav-with-image">
+							<div class="mkdf-single-nav-image-holder">
+								<a href="<?php echo $portfolio[$prev]['friendly_url'];?>"> 
+									<img width="150" height="150" src="<?php echo $md_portfolio_img[$prev_img]['img_url'];?>" class="attachment-thumbnail size-thumbnail wp-post-image" alt="<?php echo $md_portfolio_img[$prev_img]['img_alt'];?>" /> 
+								</a>
+							</div>
+							<div class="mkdf-single-nav-content-holder">
+								<div class="h6"><a href="<?php echo $portfolio[$prev]['friendly_url'];?>"><?php echo $portfolio[$prev]['meta_h1'];?></a></div>
+								<a class="mkdf-single-nav-label-holder" href="<?php echo $portfolio[$prev]['friendly_url'];?>" title="<?php echo $portfolio[$prev]['meta_h1'];?>"> 
+									<span class="mkdf-single-nav-arrow">
+										<span aria-hidden="true" class="mkdf-icon-font-elegant arrow_carrot-left_alt2 "></span> 
+									</span> 
+									<span class="mkdf-single-nav-label">Предыдущий</span> 
+								</a>
+							</div>
+						</div>
+						<div class="mkdf-portfolio-back-btn">
+							<a href="/portfolio/"> <span class="fa fa-th-large"></span> </a>
+						</div>
+						<div class="mkdf-portfolio-next mkdf-single-nav-with-image">
+							<div class="mkdf-single-nav-image-holder">
+								<a href="<?php echo $portfolio[$next]['friendly_url'];?>"> 
+									<img width="150" height="150" src="<?php echo $md_portfolio_img[$next_img]['img_url'];?>" class="attachment-thumbnail size-thumbnail wp-post-image" alt="<?php echo $md_portfolio_img[$next_img]['img_alt'];?>" /> 
+								</a>
+							</div>
+							<div class="mkdf-single-nav-content-holder">
+								<div class="h6"><a href="<?php echo $portfolio[$next]['friendly_url'];?>"><?php echo $portfolio[$next]['meta_h1'];?></a></div>
+								<a class="mkdf-single-nav-label-holder" href="<?php echo $portfolio[$next]['friendly_url'];?>" title="<?php echo $portfolio[$next]['meta_h1'];?>"> 
+									<span class="mkdf-single-nav-label">Следующий</span> 
+									<span class="mkdf-single-nav-arrow">
+										<span aria-hidden="true" class="mkdf-icon-font-elegant arrow_carrot-right_alt2 "></span> 
+									</span>
+								</a>
+							</div>
+						</div>
+-->
