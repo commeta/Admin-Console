8b139f00d3bf1628b8fc4bf804a2e3a94312f00c
Evgenii <dcs-spb@ya.ru>
2020-04-16 09:41:21 +0300

    Stable update

--- templates/chanks/blog-item.php
+++ templates/chanks/blog-item.php
@@ -13,7 +13,6 @@ else $prev= $count_blog - 1;
 if($current < $count_blog - 1) $next= $current + 1;
 else $next= 0;
 
-
 // Категории
 $category= [];
 foreach($blog as $v){
@@ -21,72 +20,58 @@ foreach($blog as $v){
 }
 ?>
 
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic"><?=$meta_h1?></h1>
-        </div>
-      </div>
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+	<div class="col-md-6 px-0">
+		<h1 class="display-4 font-italic"><?=$meta_h1?></h1>
+	</div>
+</div>
 
     
 <div class="container">
-      <div class="row mb-2">
-        <div class="col-md-6">
-          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary"><?=$blog[$prev]['category']?></strong>
-              <h3 class="mb-0">
-				  <a class="text-dark" href="<?=$blog[$prev]['friendly_url']?>"><?=$blog[$prev]['meta_h1']?></a>
-              </h3>
-              <div class="mb-1 text-muted"><?=strftime('%B %Y',strtotime($blog[$prev]['public_time']))?></div>
-              <p class="card-text mb-auto"><?=$blog[$prev]['meta_title']?></p>
-              <a href="<?=$blog[$prev]['friendly_url']?>">Продолжить чтение</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-success"><?=$blog[$next]['category']?></strong>
-              <h3 class="mb-0">
-				  <a class="text-dark" href="<?=$blog[$next]['friendly_url']?>"><?=$blog[$next]['meta_h1']?></a>
-              </h3>
-              <div class="mb-1 text-muted"><?=strftime('%B %Y',strtotime($blog[$next]['public_time']))?></div>
-              <p class="card-text mb-auto"><?=$blog[$next]['meta_title']?></p>
-              <a href="<?=$blog[$next]['friendly_url']?>">Продолжить чтение</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
-          </div>
-        </div>
-      </div>
+	
+	<div class="row mb-2">
+		<div class="col-md-6">
+			<div class="card flex-md-row mb-4 shadow-sm h-md-250">
+				<div class="card-body d-flex flex-column align-items-start">
+					<strong class="d-inline-block mb-2 text-primary"><?=$blog[$prev]['category']?></strong>
+					<h3 class="mb-0"><a class="text-dark" href="<?=$blog[$prev]['friendly_url']?>"><?=$blog[$prev]['meta_h1']?></a></h3>
+					<div class="mb-1 text-muted"><?=strftime('%B %Y',strtotime($blog[$prev]['public_time']))?></div>
+					<p class="card-text mb-auto"><?=$blog[$prev]['meta_title']?></p>
+					<a href="<?=$blog[$prev]['friendly_url']?>">Продолжить чтение</a>
+				</div>
+				<img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="card flex-md-row mb-4 shadow-sm h-md-250">
+				<div class="card-body d-flex flex-column align-items-start">
+					<strong class="d-inline-block mb-2 text-success"><?=$blog[$next]['category']?></strong>
+					<h3 class="mb-0"><a class="text-dark" href="<?=$blog[$next]['friendly_url']?>"><?=$blog[$next]['meta_h1']?></a></h3>
+					<div class="mb-1 text-muted"><?=strftime('%B %Y',strtotime($blog[$next]['public_time']))?></div>
+					<p class="card-text mb-auto"><?=$blog[$next]['meta_title']?></p>
+					<a href="<?=$blog[$next]['friendly_url']?>">Продолжить чтение</a>
+				</div>
+				<img class="card-img-right flex-auto d-none d-lg-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
+			</div>
+		</div>
+	</div>
 	
 	
-		
-	
-	
-	
-      <div class="row">		  
-        <div class="col-md-8 blog-main">
-          <h3 class="pb-3 mb-4 font-italic border-bottom"><?=$md_blog['meta_h1']?></h3>
+	<div class="row">		  
+		<div class="col-md-8 blog-main">
+			<h3 class="pb-3 mb-4 font-italic border-bottom"><?=$md_blog['meta_h1']?></h3>
 			<?=$md_blog['content']?>
-        </div><!-- /.blog-main -->
+		</div><!-- /.blog-main -->
 
+		<aside class="col-md-4 blog-sidebar">
+			<div class="p-3 mb-3 bg-light rounded">
+				<h4 class="font-italic">Описание</h4>
+				<p class="mb-0"><em>И нет сомнений,</em> что предприниматели в сети интернет объединены в целые кластеры себе подобных.</p>
+			</div>
 
-
-
-
-
-
-
-        <aside class="col-md-4 blog-sidebar">
-          <div class="p-3 mb-3 bg-light rounded">
-            <h4 class="font-italic">Описание</h4>
-            <p class="mb-0"><em>И нет сомнений,</em> что предприниматели в сети интернет объединены в целые кластеры себе подобных.</p>
-          </div>
-
-          <div class="p-3">
-            <h4 class="font-italic">Категории</h4>
-            <ol class="list-unstyled mb-0">
+			<div class="p-3">
+				<h4 class="font-italic">Категории</h4>
+				<ol class="list-unstyled mb-0">
 <?php
 foreach($category as $cat){ // Вывод ссылок постов блога, по категориям
 	$sub_cat= '';
@@ -115,8 +100,8 @@ foreach($category as $cat){ // Вывод ссылок постов блога, 
 	}
 }
 ?>
-            </ol>
-          </div>
-        </aside><!-- /.blog-sidebar -->
-      </div><!-- /.row -->
+				</ol>
+			</div>
+		</aside><!-- /.blog-sidebar -->
+	</div><!-- /.row -->
 </div>
--- templates/chanks/blog.php
+++ templates/chanks/blog.php
@@ -10,51 +10,45 @@ foreach($blog as $v){
 }
 ?>
 
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic"><?=$meta_h1?></h1>
-          <p class="lead my-3"><?=$md_meta['meta_text']?></p>
-          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Продолжить чтение...</a></p>
-        </div>
-      </div>
-
-    
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+	<div class="col-md-6 px-0">
+		<h1 class="display-4 font-italic"><?=$meta_h1?></h1>
+		<p class="lead my-3"><?=$md_meta['meta_text']?></p>
+		<p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Продолжить чтение...</a></p>
+	</div>
+</div>
 
 <div class="container">
-      <div class="row">		  
-        <div class="col-md-8 blog-main">
-          <h3 class="pb-3 mb-4 font-italic border-bottom"><?=$md_meta['meta_title']?></h3>
+	<div class="row">		  
+		<div class="col-md-8 blog-main">
+			<h3 class="pb-3 mb-4 font-italic border-bottom"><?=$md_meta['meta_title']?></h3>
 <?php
 foreach($blog as $post){ // Вывод анонсов постов блога
 	$post_public_time= strftime('%e %B %Y',strtotime($post['public_time']));
 	echo <<<ARTICLE
-          <div class="blog-post">
-            <h2 class="blog-post-title">{$post['meta_h1']}</h2>
-            <p class="blog-post-meta">$post_public_time от <a href="#">Автор</a></p>
-
-            {$post['meta_text']}
-          </div><!-- /.blog-post -->
+			<div class="blog-post">
+				<h2 class="blog-post-title">{$post['meta_h1']}</h2>
+				<p class="blog-post-meta">$post_public_time от <a href="#">Автор</a></p>
+				{$post['meta_text']}
+			</div><!-- /.blog-post -->
 ARTICLE;
 }
 ?>		
 
-          <nav class="blog-pagination">
-            <a class="btn btn-outline-primary" href="#">Старее</a>
-            <a class="btn btn-outline-secondary disabled" href="#">Новее</a>
-          </nav>
+			<nav class="blog-pagination">
+				<a class="btn btn-outline-primary" href="#">Старее</a>
+				<a class="btn btn-outline-secondary disabled" href="#">Новее</a>
+			</nav>
+		</div><!-- /.blog-main -->
+		<aside class="col-md-4 blog-sidebar">
+			<div class="p-3 mb-3 bg-light rounded">
+				<h4 class="font-italic">Описание</h4>
+				<p class="mb-0"><em>И нет сомнений,</em> что предприниматели в сети интернет объединены в целые кластеры себе подобных.</p>
+			</div>
 
-        </div><!-- /.blog-main -->
-
-
-        <aside class="col-md-4 blog-sidebar">
-          <div class="p-3 mb-3 bg-light rounded">
-            <h4 class="font-italic">Описание</h4>
-            <p class="mb-0"><em>И нет сомнений,</em> что предприниматели в сети интернет объединены в целые кластеры себе подобных.</p>
-          </div>
-
-          <div class="p-3">
-            <h4 class="font-italic">Категории</h4>
-            <ol class="list-unstyled mb-0">
+			<div class="p-3">
+				<h4 class="font-italic">Категории</h4>
+				<ol class="list-unstyled mb-0">
 <?php
 foreach($category as $cat){ // Вывод ссылок постов блога, по категориям
 	$sub_cat= '';
@@ -83,8 +77,8 @@ foreach($category as $cat){ // Вывод ссылок постов блога, 
 	}
 }
 ?>
-            </ol>
-          </div>
-        </aside><!-- /.blog-sidebar -->
-      </div><!-- /.row -->
+				</ol>
+			</div>
+		</aside><!-- /.blog-sidebar -->
+	</div><!-- /.row -->
 </div>
