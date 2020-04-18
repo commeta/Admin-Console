c69e719d6706dbbebd65a10d709dd4ccbae59b7b
Evgenii <dcs-spb@ya.ru>
2020-04-18 06:29:53 +0300

    Stable update

--- templates/index.php
+++ templates/index.php
@@ -1,4 +1,6 @@
 <?php
+# Реализация логики главной страницы
+
 // Если есть то отдаем из кэша страницу, и обработаем заголовок if modified since
 if($cached_page = get_cached_page( $urlMd5 )){
 	ifMofifiedSince( $urlMd5 );
@@ -18,10 +20,12 @@ require_once('chanks/header.php');
 
 	<div id="myCarousel" class="carousel slide" data-ride="carousel">
 		<ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-			<li data-target="#myCarousel" data-slide-to="2"></li>
-			<li data-target="#myCarousel" data-slide-to="3"></li>
+<?php
+foreach($slider as $k=>$slide){
+	$active= $k == 0 ? 'class="active"' : '';
+	printf('<li data-target="#myCarousel" data-slide-to="%d" %s></li>',$k,$active);
+}
+?>
 		</ol>
 		<div class="carousel-inner">
 <?php
@@ -44,77 +48,8 @@ SLIDE;
 			  ================================================== -->
 	<!-- Wrap the rest of the page in another container to center all the content. -->
 	<div class="container marketing">
-		<!-- Three columns of text below the carousel -->
-		<div class="row">
-			<div class="col-lg-4"> <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				<h2>Разработка</h2>
-				<p>Каждый из нас понимает очевидную вещь: курс на социально-ориентированный национальный проект в значительной степени обусловливает важность дальнейших направлений развития.</p>
-				<p><a class="btn btn-secondary" href="#" role="button">Подробнее &raquo;</a></p>
-			</div>
-			<!-- /.col-lg-4 -->
-			<div class="col-lg-4"> <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				<h2>Оптимизация</h2>
-				<p>Противоположная точка зрения подразумевает, что интерактивные прототипы представляют собой не что иное, как квинтэссенцию победы маркетинга над разумом и должны быть ограничены.</p>
-				<p><a class="btn btn-secondary" href="#" role="button">Подробнее &raquo;</a></p>
-			</div>
-			<!-- /.col-lg-4 -->
-			<div class="col-lg-4"> <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				<h2>Продвижение</h2>
-				<p>Противоположная точка зрения подразумевает, что активно развивающиеся страны третьего мира и по сей день остаются уделом либералов, которые жаждут быть превращены в посмешище.</p>
-				<p><a class="btn btn-secondary" href="#" role="button">Подробнее &raquo;</a></p>
-			</div>
-			<!-- /.col-lg-4 -->
-		</div>
-		<!-- /.row -->
-		<!-- START THE FEATURETTES -->
-		<hr class="featurette-divider">
-		<div class="row featurette">
-			<div class="col-md-7">
-				<h2 class="featurette-heading">Интернет маркетинг.</h2>
-				<p class="lead">Являясь всего лишь частью общей картины, сделанные на базе интернет-аналитики выводы являются только методом политического участия и ограничены исключительно образом мышления.</p>
-			</div>
-			<div class="col-md-5">
-				<a data-fancybox="gallery" href="/img/uploads/seo_vse_napravlenia_rabot_nad_proektom.jpg">
-					<img class="featurette-image img-fluid mx-auto" src="/img/uploads/seo_vse_napravlenia_rabot_nad_proektom.jpg" alt="Generic placeholder image">
-				</a>
-			</div>
-		</div>
-		<hr class="featurette-divider">
-		<div class="row featurette">
-			<div class="col-md-7 order-md-2">
-				<h2 class="featurette-heading">Разработка шаблона.</h2>
-				<p class="lead">Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: постоянное информационно-пропагандистское обеспечение нашей деятельности обеспечивает широкому кругу (специалистов) участие в формировании инновационных методов управления процессами.</p>
-			</div>
-			<div class="col-md-5 order-md-1">
-				<a data-fancybox="gallery" href="/img/uploads/CSS_Shorthand_Cheat_Sheet.jpg">
-					<img class="featurette-image img-fluid mx-auto" src="/img/uploads/CSS_Shorthand_Cheat_Sheet.jpg" alt="Generic placeholder image">
-				</a>
-			</div>
-		</div>
-		<hr class="featurette-divider">
-		<div class="row featurette">
-			<div class="col-md-7">
-				<h2 class="featurette-heading">Настройка сервера.</h2>
-				<p class="lead">Мы вынуждены отталкиваться от того, что постоянный количественный рост и сфера нашей активности однозначно определяет каждого участника как способного принимать собственные решения касаемо направлений прогрессивного развития.</p>
-			</div>
-			<div class="col-md-5">
-				<a data-fancybox="gallery" href="/img/uploads/JatuTLPbK7w.jpg">
-					<img class="featurette-image img-fluid mx-auto" src="/img/uploads/JatuTLPbK7w.jpg" alt="Generic placeholder image">
-				</a>
-			</div>
-		</div>
-		<hr class="featurette-divider">
-		<!--
-			<div class="row featurette">
-				<div class="col-md-7">
-					<h2 class="featurette-heading">Последний заголовок.</h2>
-					<p class="lead">Мы вынуждены отталкиваться от того, что постоянный количественный рост и сфера нашей активности однозначно определяет каждого участника как способного принимать собственные решения касаемо направлений прогрессивного развития.</p>
-				</div>
-				<div class="col-md-5"> <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image"> </div>
-			</div>
-			<hr class="featurette-divider">
-			-->
-		<!-- /END THE FEATURETTES -->
+<?=$md_meta['meta_text']?>
+<?=$md_meta['content']?>
 	</div>
 	<!-- /.container -->
 </main>
